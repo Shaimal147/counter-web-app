9e0a2fd1db6c0ffd07773560769c4ac4208de309
Shaimal <shaimalabdulla35@gmail.com>
2026-02-04 23:00:43 +0500

    Improve save logic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function createCounter() {
     let count = 0
+    let countHistory = []
     let countEl = document.getElementById("count-el")
+    let saveEl = document.getElementById("save-el")
 
     function increment() {
         count += 1
@@ -8,7 +10,10 @@ function createCounter() {
     }
 
     function save() {
-        console.log(`The current count is ${count}.`)
+        countHistory.push(count)
+        saveEl.innerText = `Previous entries: ${countHistory.join(" - ")}`
+        count = 0
+        countEl.innerText = 0
     }
 
     return {
